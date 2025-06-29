d769bf9fcd3b2958beef1acca6726508ae393905
Pradipkumar Desai <pdesai.programming@gmail.com>
2025-06-29 15:38:13 +0530

    Titanic classification model working

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hello python')
+from titanic_classification import get_titanic_model_pred
+import pandas as pd
+
+csv_path = 'data/titanic.csv'
+sample = pd.DataFrame([{
+    'Sex':'male',
+    'Age':45,
+    'Pclass':1
+}])
+pred = get_titanic_model_pred(csv_path,sample)
+print(pred)
